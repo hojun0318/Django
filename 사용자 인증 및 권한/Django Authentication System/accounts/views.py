907e3e852d214c2c6e98eeace076e2209088c8c0
hojun0318 <jaden8994@gmail.com>
2022-09-09 17:30:49 +0900

    Django Authentication System

---
+++ 사용자 인증 및 권한/Django Authentication System/accounts/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+
+# 1. 회원 가입
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+# 2. 로그인
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+# 3. 로그아웃
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+# 4. 회원정보 수정
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+# 5. 회원 탈퇴
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('articles:index')
